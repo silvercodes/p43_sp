d5ec28abe2a8ec5ac49b30f9266ed0fe9a66efb5
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-14 23:44:08 +0300

    add: process

---
+++ 01_process/01_process/Program.cs
@@ -0,0 +1,67 @@
+﻿
+#region Process
+
+// 1. Memory scope
+
+// 2. Исполняемый код
+
+// 3. Сиситемные дискрипторы
+
+// 4. Констекст безопасности
+
+// 5. Идентификатор
+
+// 6. Переменные окружения
+
+// 7. Приоритет
+
+// 8. Поток выполнения (как минимум 1)
+
+
+
+using System.Diagnostics;
+
+//Process[] processes = Process.GetProcesses();
+//IEnumerable<Process> proc = processes.OrderBy(p => p.Id);
+
+//foreach (Process p in proc)
+//    Console.WriteLine($"id: {p.Id} {p.ProcessName}");
+
+
+
+
+Console.Write("Enter PID: ");
+string? input = Console.ReadLine();
+
+try
+{
+    int pid = int.Parse(input);
+
+    Process p = Process.GetProcessById(pid);
+
+    var threads = p.Threads;
+    Console.WriteLine("Thread list:");
+    foreach (ProcessThread t in threads)
+        Console.WriteLine($"{t.Id}\t{t.StartTime.ToShortTimeString()}\t{t.PriorityLevel}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"ERROR: {ex.Message}");
+}
+
+
+
+
+
+
+
+
+#endregion
+
+
+
+
+
+
+
+
