cd7a1484a79c327f7d8a7c4a3544f4ceda58b55e
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2026-05-29 23:00:41 +0300

    add: thread, sync

--- 02_thread/01_thread/Program.cs
+++ 02_thread/01_thread/Program.cs
@@ -26,19 +26,159 @@
 #region Intro
 
 
-void RenderPlus()
+//void RenderPlus()
+//{
+//    for (int i = 0; i < 1000; i++)
+//        Console.Write('+');
+//}
+
+//Thread t = new Thread(RenderPlus);
+//t.Start();
+
+//for (int i = 0; i < 1000; i++)
+//    Console.Write('0');
+
+
+
+//void Run()
+//{
+//    for (int i = 0; i < 5; i++)
+//        Console.Write('0');
+//}
+
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+// ============  ПотокоНЕбезобасный код 
+//bool done = false;
+
+//void Run()
+//{
+//    if (!done)
+//    {
+//        Console.WriteLine('*');
+//        done = true;
+//        Console.WriteLine("DONE");
+//    }
+//}
+
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+// ============  Потокобезобасный код
+//object locker = new object();
+//bool done = false;
+
+//void Run()
+//{
+//    lock(locker)
+//    {
+//        if (!done)
+//        {
+//            Console.WriteLine('*');
+//            done = true;
+//            Console.WriteLine("DONE");
+//        }
+//    }
+//}
+
+//new Thread(Run).Start();
+//Run();
+
+
+
+
+
+
+//void Run()
+//{
+//    for (int i = 0; i < 1000; ++i)
+//        Console.Write('*');
+//    Console.WriteLine();
+//}
+
+//Console.WriteLine("Main start");
+//Thread t = new Thread(Run);
+//t.Start();
+
+//// Thread.Sleep(1);
+//t.Join();               // Блокируем текущий поток (ждём завершения потока t)
+
+//Console.WriteLine("Main end");
+
+#endregion
+
+
+
+#region Create / Start
+
+// ============ Простые способы ============
+//void Run()
+//{
+//    Console.WriteLine("hello");
+//}
+
+//Thread t = new Thread(new ThreadStart(Run));
+//t.Start();
+//Run();
+
+
+
+
+//Thread t = new Thread(() => Console.WriteLine("Vasia"));
+//t.Start();
+
+
+
+
+//string email = "vasia@mail.com";
+
+//// Thread t = new Thread(new ThreadStart(() => Console.WriteLine($"EMAIL: {email}")));
+//// >>> EQUALS <<<
+//Thread t = new Thread(() => Console.WriteLine($"EMAIL: {email}"));
+//t.Start();
+
+
+
+
+
+void Calc(int a, int b)
 {
-    for (int i = 0; i < 1000; i++)
-        Console.Write('+');
+    Console.WriteLine($"RESULT = {a + b}");
 }
 
-Thread t = new Thread(RenderPlus);
+int a = 30;
+int b = 4;
+
+//--- Способ 1
+
+//void RunCalc(object? obj)
+//{
+//    if (obj is Parameters p)
+//        Calc(p.x, p.y);
+//}
+
+//Thread t = new Thread(RunCalc);
+//t.Start(new Parameters() { x = a, y = b });
+
+//class Parameters
+//{
+//    public int x;
+//    public int y;
+//}
+
+
+//--- Способ 2
+Thread t = new Thread(() => Calc(a, b));
 t.Start();
-
-for (int i = 0; i < 1000; i++)
-    Console.Write('0');
-
-
 
 
 #endregion
+
+
